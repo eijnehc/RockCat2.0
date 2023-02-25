a10d1cdb6a2e9f876cd4a3150855c012073497e6
Chen Jie <61531625+eijnehc@users.noreply.github.com>
2023-02-25 18:51:32 +0800

    Build specific confic

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,16 +2,26 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:8000',
-        // changeOrigin: true,
-        // secure: false,
-        // ws: true,
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    // dev specific config
+    return {
+      plugins: [react()],
+      server: {
+        proxy: {
+          '/api/v1': {
+            target: 'http://localhost:8000',
+            // changeOrigin: true,
+            // secure: false,
+            // ws: true,
+          },
+        },
       },
-    },
-  },
+    }
+  } else {
+    // build specific config
+    return {
+      plugins: [react()],
+    }
+  }
 })
